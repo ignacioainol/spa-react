5f553a9792fd666c980c34c487b7cdbec6c0c968
ignacioainol <iggnaxios@gmail.com>
2021-03-02 14:01:08 -0300

    search hero by name ok

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useLocation } from 'react-router-dom';
 import { heroes } from '../../data/heroes'
 import { useForm } from '../../hooks/useForm';
+import queryString from 'query-string'
 import { HeroCard } from '../hero/HeroCard';
+import { getHeroesByname } from '../../selectors/getHeroesByName';
 
-export const SearchScreen = () => {
+export const SearchScreen = ({ history }) => {
 
-    const heroesFiltered = heroes;
+    const location = useLocation();
+    const { q = '' } = queryString.parse(location.search);
+
     const [formValues, handleInputChange] = useForm({
-        searchText: ''
+        searchText: q
     });
 
     const { searchText } = formValues;
 
+    const heroesFiltered = useMemo(() => getHeroesByname(q), [q]);
+
     const handleSearch = (e) => {
         e.preventDefault();
-        console.log(searchText);
+        history.push(`?q=${searchText}`);
     }
 
     return (
@@ -45,6 +52,16 @@ export const SearchScreen = () => {
                 </div>
                 <div className="col-7">
                     <h4>Results</h4>
+                    <br />
+
+                    {
+                        (q === '') ? <div className="alert alert-info">
+                            Search a Hero
+                            </div> : (heroesFiltered.length < 1) && <div className="alert alert-warning">
+                                There is not a hero with {q}
+                            </div>
+                    }
+
 
                     {
                         heroesFiltered.map(hero => (
