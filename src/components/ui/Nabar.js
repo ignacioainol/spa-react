2c0446990ad8811303ee1eb65fa99bcf365b7193
ignacioainol <iggnaxios@gmail.com>
2021-03-03 23:54:45 -0300

    useContext implemented

--- src/components/ui/Nabar.js
+++ src/components/ui/Nabar.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { AuthContext } from '../../auth/AuthContext'
 
 export const Navbar = () => {
+
+    const { user: { name } } = useContext(AuthContext);
+    console.log(name);
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
@@ -33,6 +38,7 @@ export const Navbar = () => {
                         DC
                     </NavLink>
 
+
                     <NavLink
                         activeClassName="active"
                         className="nav-item nav-link"
@@ -47,14 +53,16 @@ export const Navbar = () => {
             {/* <div className="navbar-collapse collapse w-100 order-3 dual-collapse2"> */}
             <div>
                 <ul className="navbar-nav ml-auto">
-                    <NavLink
-                        activeClassName="active"
+
+                    <span className="nav-item nav-link text-info">
+                        {name}
+                    </span>
+
+                    <button
                         className="nav-item nav-link"
-                        exact
-                        to="/login"
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
